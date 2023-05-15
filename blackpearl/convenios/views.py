82858a9985e7ef3ee4a641f1d556d8581fbd7dc3
Maxson Ferovante <maxsonferovante@gmail.com>
2023-05-14 21:36:20 -0300

    Exportação de faturas com filtros, formados: pdf, xlsx e txt

--- blackpearl/convenios/views.py
+++ blackpearl/convenios/views.py
@@ -1,5 +1,5 @@
 from io import BytesIO
-
+import openpyxl
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -111,37 +111,76 @@ def listarFaturas(request):
 
 
 def exporttofile(faturas, nome_arq,tipoArquivo_selecionado):
+    filename = f"{nome_arq}"
 
-    quant_faturas = faturas.count()
-    buffer = BytesIO()
-    pdf = canvas.Canvas(buffer)
-    y = 750  # Posição y inicial
-    for fatura in faturas:
-        if y<=50:
-            pdf.showPage()
-            y = 750
-        # Escreve o título da fatura
-        pdf.drawString(100, y, "Titular do Cartão: {}".format(fatura.cartao.titular.nomecompleto))
-        y -= 20  # Move para a próxima linha
-        pdf.drawString(100, y, "Empresa: {}".format(fatura.cartao.titular.empresa.nome))
-        y -= 20  # Move para a próxima linha
-        # Escreve as informações da fatura
-        pdf.drawString(100, y, "Competência: {}".format(fatura.competencia))
-        y -= 20
-        pdf.drawString(100, y, "Valor da fatura: {}".format(fatura.valor))
-        y -= 20
-        pdf.drawString(100, y, "Valor com a taxa administrativa: {}".format(fatura.valorComTaxa))
-        y -= 40  # Move duas linhas para baixo
+    if tipoArquivo_selecionado == '1':
+        quant_faturas = faturas.count()
+        buffer = BytesIO()
+        pdf = canvas.Canvas(buffer)
+        y = 750  # Posição y inicial
+        for fatura in faturas:
+            if y<=50:
+                pdf.showPage()
+                y = 750
+            # Escreve o título da fatura
+            pdf.drawString(100, y, "Titular do Cartão: {}".format(fatura.cartao.titular.nomecompleto))
+            y -= 20  # Move para a próxima linha
+            pdf.drawString(100, y, "Empresa: {}".format(fatura.cartao.titular.empresa.nome))
+            y -= 20  # Move para a próxima linha
+            # Escreve as informações da fatura
+            pdf.drawString(100, y, "Competência: {}".format(fatura.competencia))
+            y -= 20
+            pdf.drawString(100, y, "Valor da fatura: {}".format(fatura.valor))
+            y -= 20
+            pdf.drawString(100, y, "Valor com a taxa administrativa: {}".format(fatura.valorComTaxa))
+            y -= 40  # Move duas linhas para baixo
 
-    pdf.save()
-    # Define o nome do arquivo PDF
-    filename = f"{nome_arq}.pdf"
-    # Envia o PDF para o navegador como um arquivo de download
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = f'attachment; filename="{filename}"'
-    buffer.seek(0)
-    response.write(buffer.read())
-    buffer.close()
+        pdf.save()
+        # Define o nome do arquivo PDF
+        filename = filename + ".pdf"
+        # Envia o PDF para o navegador como um arquivo de download
+        response = HttpResponse(content_type='application/pdf')
+
+        response['Content-Disposition'] = f'attachment; filename="{filename}"'
+        buffer.seek(0)
+
+        response.write(buffer.read())
+
+        buffer.close()
+    elif tipoArquivo_selecionado  == '2':
+        wb = openpyxl.Workbook()
+        ws = wb.active
+        ws.title = nome_arq
+        # Escreve os dados da lista de faturas na planilha
+        ws['A1'] = "Titular"
+        ws['B1'] = "Empresa"
+        ws['C1'] = "Competêcia"
+        ws['D1'] = "Valor"
+        ws['E1'] = "Valor com taxa"
+
+        for i, fatura in enumerate(faturas):
+            ws.cell(row=i + 2, column=1, value=fatura.cartao.titular.nomecompleto)
+            ws.cell(row=i + 2, column=2, value=fatura.cartao.titular.empresa.nome)
+            ws.cell(row=i + 2, column=3, value=fatura.competencia)
+            ws.cell(row=i + 2, column=4, value=fatura.valor)
+            ws.cell(row=i + 2, column=5, value=fatura.valorComTaxa)
+
+        # Cria uma resposta HTTP com o arquivo XLSX
+        response = HttpResponse(content_type='application/xlsx')
+        response['Content-Disposition'] = f'attachment; filename={nome_arq}.xlsx'
+        wb.save(response)
+
+
+    elif tipoArquivo_selecionado == '3':
+        faturas_txt = ''
+        for fatura in faturas:
+            matricula = str(fatura.cartao.titular.matricula).zfill(6)
+            valorComTaxa = str(fatura.valorComTaxa)[:4].replace('.','').zfill(12)
+            faturas_txt += '{} {}\n'.format(matricula,valorComTaxa)
+
+        response = HttpResponse(faturas_txt, content_type='text/plain')
+        response['Content-Disposition'] = f'attachment; filename="{nome_arq}.txt'
+
     return response
 
 

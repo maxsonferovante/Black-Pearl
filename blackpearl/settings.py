2943e7c02cb130aaf27d8b7d2bdb194d908a6215
Maxson Ferovante <maxsonferovante@gmail.com>
2023-05-21 15:05:42 -0300

    configurações iniciais para postgresql

--- blackpearl/settings.py
+++ blackpearl/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'blackpearl.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blackpearl',
+        'USER': 'maxson',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 

3fa84087a63d0c0bf8868589006d5dad462fad32
Maxson Ferovante <maxsonferovante@gmail.com>
2023-05-29 04:07:12 -0300

    fix - correção de commit

--- blackpearl/convenios/views.py
+++ blackpearl/convenios/views.py
@@ -197,6 +197,7 @@ class ContratoPlanoOdontologicoWizardView(SessionWizardView):
                 form.save()
         else:
             contratante_form.save()
+
         return redirect('listagemcontratoodontologica')
 
 

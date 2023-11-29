a257382df092b9aba58209b50d368ce5fc0600c6
Maxson Ferovante <maxsonferovante@users.noreply.github.com>
2023-11-28 23:00:17 -0300

    remoção do fatura plano de saude

--- blackpearl/cobrancas/admin.py
+++ blackpearl/cobrancas/admin.py
@@ -1,16 +1,2 @@
 from django.contrib import admin
-from blackpearl.cobrancas.models.faturasConvenios import FaturaPlanoSaude
 # Register your models here.
-
-
-@admin.register(FaturaPlanoSaude)
-class FaturaPlanoSaudeAdmin(admin.ModelAdmin):
-    list_display = ('id',
-                    'nome',
-                    'competencia',
-                    'valorTotalContratos',
-                    'valorTotalAtendimentoDomiciliar',
-                    'valorTotalAtendimentoTelefonico',
-                    'valorTotalTaxaAdministrativa',
-                    'valorTotal',
-                    'ativo')
--- blackpearl/cobrancas/views/homeView.py
+++ blackpearl/cobrancas/views/homeView.py
@@ -3,7 +3,6 @@ from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
 from blackpearl.convenios.models.planoSaudeModels import ContratoPlanoSaude, ContratoPlanoSaudeDependente
-from blackpearl.cobrancas.services.relatoriosConvenios import RelatoriosConvenios
 
 
 
@@ -11,13 +10,6 @@ from blackpearl.cobrancas.services.relatoriosConvenios import RelatoriosConvenio
 @method_decorator(login_required, name='dispatch')
 class HomeTemplateView(TemplateView):
     template_name = 'home_cob.html'
-
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        relatorios = RelatoriosConvenios(ContratoPlanoSaude)
-
-        context['ativos_e_inativos'] = relatorios.ativosEInativos()
-        context['total_por_associacao'] = relatorios.totalPorAssociacao()
-
         return context
